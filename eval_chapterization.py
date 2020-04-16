44978e7c54d41c090546aa719e614db7980f1b1e
lukas <lm@netzlabor.de>
2020-04-16 17:38:49 +0200

    [WIP] add segmentation evaluation and parameter matrix functions

--- eval_chapterization.py
+++ eval_chapterization.py
@@ -1,25 +1,55 @@
 # script to automatically evaluate different sets of parameters in the chapterization process
 
-import importlib
+import os, fasttext, fasttext.util, segeval
+
+# change sys path to allow realtive imports of podcast-chapterize modules
+import sys
+sys.path.append('./podcast-chapterize')
+from chapterize.preprocessor_helper import lemma
 
 def eval():
+
     transcripts = get_transcripts()
 
-    print(get_true_chapter_boundaries(transcripts[0]))
+    param_matrix = parameter_matrix()
 
-    parameter_matrix()
+    # load ft models once globally to prevent need for loading the models multiple times
+    model_path = fasttext.util.download_model('en', if_exists='ignore')
+    ft_en = fasttext.load_model(model_path)
+
+    model_path = fasttext.util.download_model('de', if_exists='ignore')
+    ft_de = fasttext.load_model(model_path) 
+
+    eval_score_matrix = [[] for i, x in enumerate(param_matrix)] # transcript scores by parameter set
+
+    for i, transcript in enumerate(transcripts):
+        true_chapter_boundaries = get_true_chapter_boundaries(transcript)
+
+        # bulk lemmatize to prevent redundancy 
+        transcript['tokens'] = lemmatize(transcript['tokens'], transcript['language'])
+
+        for j, params in enumerate(param_matrix):
+            print(f'test transcript {i} with parameters {j}')
+            boundaries = run_chapterization(transcript, params, ft_en, ft_de)
+
+            score = eval_segmentation(boundaries, true_chapter_boundaries, len(transcript['tokens']))
+            print(f'segmentation similarity: {score}')
+            eval_score_matrix[j].append(score)
+            
+
 
 def get_transcripts():
     """fetch transcripts from json files and convert tokens to TranscriptToken objects
     """    
 
     import glob, os, json
-    TranscriptToken = importlib.import_module('podcast-chapterize.transcribe.SpeechToTextModules.SpeechToTextModule').TranscriptToken
+    from transcribe.SpeechToTextModules.SpeechToTextModule import TranscriptToken
 
     transcripts = []
     for file in glob.glob("transcripts/*.json"):
         with open(file) as f:
             print('get ', file)
+            
             transcript = json.load(f)
             transcript['tokens'] = [TranscriptToken.from_dict(token) for token in transcript['tokens']]
             transcripts.append(transcript)
@@ -46,8 +76,33 @@ def get_true_chapter_boundaries(transcript):
     return true_chapter_boundaries[1:]
 
 
+class ChapterizerParameter:
+    """class that holds parameter configuration for the chapterizer
+    """
+    def __init__(
+        self,
+        window_width,
+        with_boundaries,
+        tfidf_min_df,
+        tfidf_max_df,
+        savgol_window_length,
+        savgol_polyorder,
+        doc_vectorizer
+    ):
+        self.window_width = window_width
+        self.with_boundaries = with_boundaries
+        self.tfidf_min_df = tfidf_min_df
+        self.tfidf_max_df = tfidf_max_df
+        self.savgol_window_length = savgol_window_length
+        self.savgol_polyorder = savgol_polyorder
+        self.doc_vectorizer = doc_vectorizer
+
+
 def parameter_matrix():
-    """create a parameter matrix (list of parameter dicts)
+    """create a parameter matrix (list of parameter dicts) for every combination of possible parameters
+    
+    Returns:
+        [list of ChapterizerParameter]: parameter configurations
     """
 
     # values to test
@@ -60,7 +115,7 @@ def parameter_matrix():
     doc_vectorizer = ['ft_sif_average', 'tfidf', 'ft_average', 'ft_sum']
 
     import itertools
-    param_lists = list(itertools.product(*[
+    param_matrix = list(itertools.product(*[
         window_width,
         with_boundaries,
         tfidf_min_df,
@@ -69,54 +124,94 @@ def parameter_matrix():
         savgol_polyorder,
         doc_vectorizer
     ]))
-    keys = []
 
-    param_dicts = [{keys[i]: val for i, val in enumerate(param_vec)} for param_vec in param_lists]
-    print(l[0])
-    print
+    param_objects = [ChapterizerParameter(*params) for params in param_matrix]
 
+    return param_objects
 
+def lemmatize(tokens, language):
+    chunk_tokens_lemma = lemma([token.token for token in tokens], language)
+    for i, token in enumerate(tokens):
+        token.token = chunk_tokens_lemma[i]
+    return tokens
 
-def run_chapterization(transcript, params):
+def run_chapterization(transcript, params, ft_en, ft_de):
     """run chapterization with the given parameters
     
     Args:
         transcript (list of TranscriptToken): transcript to chapterize
         params (dict): parameters as dictionary
+
+    Returns:
+        [list of int]: identified boundaries (positon as token count)
+    """   
+
+    from chapterize.chapterizer import Chapterizer
+
+    c = Chapterizer(
+        window_width=params.window_width,
+        tfidf_min_df=params.tfidf_min_df,
+        tfidf_max_df=params.tfidf_max_df,
+        savgol_window_length=params.savgol_window_length,
+        savgol_polyorder=params.savgol_polyorder,
+        doc_vectorizer=params.doc_vectorizer
+    )
+
+    concat_chapters, boundaries = c.chapterize(
+        transcript['tokens'],
+        ft_en,
+        ft_de,
+        boundaries=[],
+        language=transcript['language'],
+        skip_lemmatization=True
+    )
+
+    return boundaries
+
+def eval_segmentation(segmentation, gold_segmentation, doc_length):
+    """scrores a segmentation compared to a gold standard segmentation
+    
+    Args:
+        segmentation (list): segmentation to score
+        gold_segmentation (list): segmentation to compare against
+        doc_length (int): length of the segmented document
+    
+    Returns:
+        float: score of segmentation
     """
 
-    Chapterizer = importlib.import_module('podcast-chapterize.chapterize.chapterizer').Chapterizer
+    #todo: gives bad output, only addition etids, how to get single score value?
+
+    boundary_string_seg = boundary_string_from_boundary_indices(segmentation, doc_length)
+    boundary_string_gold = boundary_string_from_boundary_indices(gold_segmentation, doc_length)
+
+    return segeval.boundary_edit_distance(boundary_string_seg, boundary_string_gold)
+
+def boundary_string_from_boundary_indices(segmentation, doc_length):
+    """converts boundary indices to segeval-compatible boundary strings
     
+    Args:
+        segmentation (list of int): list of segmentation boundary indices
+        doc_length (int): length of the segmented document
 
-eval()
+    Returns:
+        tuple: Boundary string
+    """
 
+    segmentation.append(doc_length)
 
+    masses = []
+    tokens_in_segment = 0
+    current_boundary_index = 0
+    for i in range(doc_length):
+        if i >= segmentation[current_boundary_index]:
+            masses.append(tokens_in_segment)
+            tokens_in_segment = 0
+            current_boundary_index += 1
+        tokens_in_segment += 1
 
+    return segeval.boundary_string_from_masses(masses)
 
-def chapterize_action(args):
-    from transcribe.SpeechToTextModules.SpeechToTextModule import TranscriptToken
-    from chapterize.chapterizer import Chapterizer
-    from chapterize.chapter_namer import chapter_names
-
-    with open(args.transcript, 'r') as f:
-        transcript = json.load(f)
-
-    tokens = [TranscriptToken(token['token'], token['time']) for token in transcript['tokens']]
-    boundaries = transcript['boundaries']
-
-    chapterizer = Chapterizer(
-        window_width=args.window_width,
-        max_utterance_delta=args.max_utterance_delta,
-        tfidf_min_df=args.tfidf_min_df,
-        tfidf_max_df=args.tfidf_max_df,
-        savgol_window_length=args.savgol_window_length,
-        savgol_polyorder=args.savgol_polyorder,
-    )
-    concat_chapters, minima = chapterizer.chapterize(tokens, boundaries, language=args.language)
-    print([f"{tokens[minimum].time}" for minimum in minima])
-
-    titles = chapter_names(concat_chapters)
-    print(titles)
 
 def set_lang(match, language):
     import glob, os, json
@@ -128,3 +223,6 @@ def set_lang(match, language):
             j['language'] = language
             with open(file, 'w') as f:
                 json.dump(j, f)
+
+
+eval()
